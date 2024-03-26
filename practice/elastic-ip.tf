a114fe8d90990b4a98d550650861b59a2418bf78
Shailndra <shailndrasingh01@gmail.com>
2024-03-27 02:22:59 +0530

    changescommit

--- practice/elastic-ip.tf
+++ practice/elastic-ip.tf
@@ -6,7 +6,7 @@ resource "aws_eip" "lb" {
 resource "aws_vpc_security_group_ingress_rule" "random" {
     security_group_id = aws_security_group.random.id
 
-    cidr_ipv4 = "${aws_eip.lb.public_ip}/32"     #cross-referencing attibutes with sring interpolation
+    cidr_ipv4 = "${aws_eip.lb.public_ip}/32"     #cross-referencing attibutes with string interpolation
     from_port = 443
     ip_protocol = "tcp"
     to_port = 443
