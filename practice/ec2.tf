574990cd7290f318795cb7f66324f2387008555c
Shailndra <shailndrasingh01@gmail.com>
2024-03-27 02:21:09 +0530

    changescommit

--- practice/ec2.tf
+++ practice/ec2.tf
@@ -4,23 +4,23 @@ resource "aws_instance" "ec2-1" {
 }
 
 resource "aws_instance" "ec2-2" {
-    instance_type = "t3a.medium"
+    instance_type = "t3a.small"
     ami = "ami-0d7a109bf30624c99"
 }
 
 # Create a new host with instance type of c5.18xlarge with Auto Placement
 # and Host Recovery enabled.
 resource "aws_ec2_host" "ec2-test" {
-  instance_type     = "c5.18xlarge"
-  availability_zone = "us-west-2a"
+  instance_type     = "t2.small"
+  availability_zone = "us-east-2a"
   host_recovery     = "on"
   auto_placement    = "on"
 }
 
 
 resource "aws_ec2_host" "test-ec2" {
-  instance_type     = "c5.18xlarge"
-  availability_zone = "us-west-2a"
+  instance_type     = "t2.micro"
+  availability_zone = "us-east-2a"
 }
 
 data "aws_ec2_host" "test" {
